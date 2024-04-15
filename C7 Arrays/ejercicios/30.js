0597ae83a0dc576122dc04d86da940d3f4fa4f0d
Max Robles <maxirobles1234@gmail.com>
2024-04-15 12:12:22 +0200

    Solve 'Ejercicios Extra' & C7/30

--- C7 Arrays/ejercicios/30.js
+++ C7 Arrays/ejercicios/30.js
@@ -2,6 +2,13 @@ function encontrarElementoRepetido(numeros) {
 	// La función recibe un argumento "numeros" que es un array de números.
 	// Retorna el primer elemento repetido que se encuentre en el array.
 	// Tu código:
+	for (let i = 0; i < numeros.length; i++) {
+		for (let a = i + 1; a < numeros.length; a++) {
+			if (numeros[i] == numeros[a]) {
+				return numeros[i];
+			}
+		}
+	}
 }
 
 module.exports = encontrarElementoRepetido;
